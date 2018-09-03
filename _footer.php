2af2e9d41ad3ee8ad3ce16ec8d75dd91c144bb08
User <User@DESKTOP-76HRC5D>
2018-09-03 22:18:26 +0530

    v4

---
+++ _footer.php
@@ -0,0 +1,5 @@
+<div class="copyright-w3layouts py-xl-3 py-2 mt-xl-5 mt-4 text-center">
+                <p>© 2018 arogya . All Rights Reserved | Design by
+                    <a href="#"> Arogya Hospitals </a>
+                </p>
+            </div>
