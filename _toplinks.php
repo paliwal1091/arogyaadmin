efa03740d60a930c69680a40b1b469056a66545e
User <User@PC-002-025>
2018-09-07 08:55:09 +0530

    login

---
+++ _toplinks.php
@@ -0,0 +1,15 @@
+ <!-- Style-sheets -->
+        <!-- Bootstrap Css -->
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <!-- Bootstrap Css -->
+        <!-- Common Css -->
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <!--// Common Css -->
+        <!-- Fontawesome Css -->
+        <link href="css/fontawesome-all.css" rel="stylesheet">
+        <!--// Fontawesome Css -->
+        <!--// Style-sheets -->
+        <!--web-fonts-->
+        <link href="//fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+        <!--//web-fonts-->
